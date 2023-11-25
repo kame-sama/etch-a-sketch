c52e6335b8804a12eec89c73e89a5bda4806fd0f
kame-sama <ad.bodeev@gmail.com>
2023-11-25 10:05:59 +0500

    Add grid size menu

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const boardSize = 480;
 const board = document.querySelector('.board');
+const newGridButton = document.querySelector('button');
 
 window.addEventListener('load', setGrid(16));
 
 board.addEventListener('mouseover', e => {
     changeCeilColor(e);
 });
+
+newGridButton.addEventListener('click', setGridSizeMenu);
 
 function setGrid(ceilsPerSide) {
     const ceilSize = boardSize / ceilsPerSide;
@@ -19,7 +22,49 @@ function setGrid(ceilsPerSide) {
     }
 }
 
+function clearGrid() {
+    const ceils = document.querySelectorAll('.ceil');
+    ceils.forEach(ceil => ceil.remove());
+}
+
 function changeCeilColor(ceil) {
     ceil.target.style.backgroundColor = 'black';
     ceil.target.style.border = 'none';
+}
+
+function setGridSizeMenu() {
+    const body = document.querySelector('body');
+    const main = document.querySelector('main');
+    const gridSizeMenu = document.createElement('div');
+
+    gridSizeMenu.classList.add('grid-size-menu');
+
+    const input = document.createElement('input');
+    input.setAttribute('type', 'number');
+    input.setAttribute('min', '16');
+    input.setAttribute('max', '100');
+    input.id = 'ceils-per-side';
+
+    const submit = document.createElement('button');
+    submit.textContent = 'Submit';
+    submit.id = 'submit';
+
+    gridSizeMenu.appendChild(input);
+    gridSizeMenu.appendChild(submit);
+
+    body.insertBefore(gridSizeMenu, main);
+
+    submit.addEventListener('click', setNewGrid);
+}
+
+function setNewGrid() {
+    const input = document.querySelector('#ceils-per-side');
+    const gridSizeMenu = document.querySelector('.grid-size-menu');
+    const ceilsPerSide = input.value;
+
+    if (ceilsPerSide >= 16 && ceilsPerSide <= 100) {
+        clearGrid();
+        setGrid(ceilsPerSide);
+        gridSizeMenu.remove();
+    }
 }
